b3190cb99621e0343eb8f30c57f1cce1ec287009
Kindra Kinney <kindrakinney@gmail.com>
2020-03-10 19:48:25 -0700

    adding initial files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(250) NOT NULL,
+    devoured BOOLEAN DEFAULT false, 
+    PRIMARY KEY(id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', false);

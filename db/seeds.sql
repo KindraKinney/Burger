a7eb2d480d95b5d1480a7b412ba88ff99b881a90
Kindra Kinney <kindrakinney@gmail.com>
2020-03-12 18:47:27 -0700

    created server and added connection

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,4 @@ USE burgers_db;
 INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', false);
+
